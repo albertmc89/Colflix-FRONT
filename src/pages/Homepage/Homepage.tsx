a1051b7a10949371a20221240f1eba66e60a7e10
Albert Colubi <albertmc89@gmail.com>
2024-01-25 17:34:10 +0100

    Add MovieCard component

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,17 +1,38 @@
 import "./Homepage.css";
 import play from "/img/play.png";
-import info from "/img/info.svg";
+import info from "/img/info.png";
 import logostrangerthings from "/img/logostrangerthings.png";
 import Navbar from "../../components/Navbar/Navbar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import paths from "../../paths/paths";
+import {
+  loadGenresActionCreator,
+  loadMoviesActionCreator,
+} from "../../store/netflix/netflix";
+import { useAppDispatch } from "../../store";
+import useNetflixApi from "../../hooks/useNetflixApi";
+import CardSlider from "../../components/CardSlider/CardSlider";
 
 const Homepage = (): React.ReactElement => {
   const [user] = useAuthState(auth);
   const [isScrolled, setIsScrolled] = useState(false);
+  const dispatch = useAppDispatch();
+  const { getGenres, getMovies } = useNetflixApi();
+
+  useEffect(() => {
+    if (user) {
+      (async () => {
+        const genres = await getGenres();
+        const movies = await getMovies();
+
+        dispatch(loadGenresActionCreator(genres!));
+        dispatch(loadMoviesActionCreator(movies!));
+      })();
+    }
+  }, [dispatch, getGenres, user, getMovies]);
 
   window.onscroll = () => {
     setIsScrolled(window.scrollY === 0 ? false : true);
@@ -28,16 +49,17 @@ const Homepage = (): React.ReactElement => {
           </div>
           <div className="button-container">
             <NavLink className="button-flex" to={paths.player}>
-              <img src={play} alt="play icon" />
+              <img src={play} alt="play icon" width="30" height="30" />
               Play
             </NavLink>
             <NavLink className="button-flex" to={paths.home}>
-              <img src={info} alt="info icon" />
+              <img src={info} alt="info icon" width="30" height="30" />
               More info
             </NavLink>
           </div>
         </article>
       </div>
+      <CardSlider />
     </>
   );
 };
