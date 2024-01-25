aee9de7c6eaff8d3a912c44c0c5910fd6b7bd164
Albert Colubi <albertmc89@gmail.com>
2024-01-25 17:40:09 +0100

    Add netflixSlice with load reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,10 @@ import {
   configureStore,
 } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { netflixReducer } from "./netflix/netflixSlice";
 
 const rootReducer = combineReducers({
-  // insert the slices reducers
+  netflixState: netflixReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
