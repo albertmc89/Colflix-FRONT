39a30d589b2b6a4d24ce20090d1b170b4718cca2
Albert Colubi <albertmc89@gmail.com>
2024-02-02 21:40:31 +0100

    Update Slider for trending

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,25 +6,49 @@ import "./Slider.css";
 const Slider = (): React.ReactElement => {
   const movies = useAppSelector((state) => state.netflixState.movies);
   const tvshows = useAppSelector((state) => state.netflixState.tvshows);
+  const trendingMovies = useAppSelector(
+    (state) => state.netflixState.trendingMovies,
+  );
+  const trendingTvShows = useAppSelector(
+    (state) => state.netflixState.trendingTvShows,
+  );
 
   return (
     <>
-      <h2 className="section__title">MOVIES</h2>
-      <ul className="movies-list">
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <MovieCard movie={movie} />
-          </li>
-        ))}
-      </ul>
-      <h2 className="section__title">TV SHOWS</h2>
-      <ul className="tv-list">
-        {tvshows.map((tvshow) => (
-          <li key={tvshow.id}>
-            <MovieCard movie={tvshow} />
-          </li>
-        ))}
-      </ul>
+      <section className="list-section">
+        <h2 className="section__title"> TRENDING MOVIES</h2>
+        <ul className="movies-list">
+          {trendingMovies.map((trend) => (
+            <li key={trend.id}>
+              <MovieCard movie={trend} />
+            </li>
+          ))}
+        </ul>
+        <h2 className="section__title">MOVIES</h2>
+        <ul className="movies-list">
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <MovieCard movie={movie} />
+            </li>
+          ))}
+        </ul>
+        <h2 className="section__title">TRENDING TV SHOWS</h2>
+        <ul className="tv-list">
+          {trendingTvShows.map((trendtv) => (
+            <li key={trendtv.id}>
+              <MovieCard movie={trendtv} />
+            </li>
+          ))}
+        </ul>
+        <h2 className="section__title">TV SHOWS</h2>
+        <ul className="tv-list">
+          {tvshows.map((tvshow) => (
+            <li key={tvshow.id}>
+              <MovieCard movie={tvshow} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };
